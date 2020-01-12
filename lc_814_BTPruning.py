f1caeb93b53f1095f21e76c838d328edc1087b7e
yoyau <yoyau.eo06g@nctu.edu.tw>
2020-01-12 13:54:40 +0800

    814 done (slow)

--- lc_814_BTPruning.py
+++ lc_814_BTPruning.py
@@ -9,3 +9,17 @@ class Solution(object):
         :type root: TreeNode
         :rtype: TreeNode
         """
+        if not root: return
+        if self.isUniZero(root): 
+            return
+        if self.isUniZero(root.left): root.left = None
+        if self.isUniZero(root.right): root.right = None
+        self.pruneTree(root.left)
+        self.pruneTree(root.right)
+        return root
+
+    def isUniZero(self, node):
+        if not node: return True
+        if node.val != 0:
+            return False
+        return self.isUniNode(node.left) and self.isUniNode(node.right)
