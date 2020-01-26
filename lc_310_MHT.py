8268a98edabfec264c3a7eda5111f7057b3edd07
yoyau <yoyau.eo06g@nctu.edu.tw>
2020-01-27 00:03:54 +0800

    lc 310 done

--- lc_310_MHT.py
+++ lc_310_MHT.py
@@ -12,6 +12,7 @@ class Solution(object):
         :type edges: List[List[int]]
         :rtype: List[int]
         """
+        # map edges to adajacency List
         adjList = [[] for i in range(n)]
         for edge in edges:
             adjList[edge[0]].append(edge[1])
@@ -20,31 +21,24 @@ class Solution(object):
         to_be_deleted = [ind for ind, length in enumerate(adjList) if len(length) == 1]
         to_be_deleted_2 = []
              
-        while to_be_deleted:
-
+        while True:
+            print(to_be_deleted)
+            print(adjList)
             if len(to_be_deleted) == 2 and adjList[to_be_deleted[0]][0] == to_be_deleted[1] and adjList[to_be_deleted[1]][0] == to_be_deleted[0]:
                 return to_be_deleted
-            i = to_be_deleted[0]
-            
-            for neighbor in adjList[i]:
-                print(adjList)
-                print(to_be_deleted)
-                print(to_be_deleted_2)
-                adjList[neighbor].remove(i)
-                del adjList[i][0]
-                
-                if len(adjList[neighbor]) == 0:
-                    return [neighbor]
-                elif len(adjList[neighbor]) == 1:
-                    to_be_deleted_2.append(neighbor)
-            del to_be_deleted[0]
-            to_be_deleted, to_be_deleted_2 = to_be_deleted_2, to_be_deleted
+            for i in to_be_deleted:
+                for neighbor in adjList[i]:
+                    adjList[neighbor].remove(i)
+                    if len(adjList[neighbor]) == 0:
+                        return [neighbor]
+                    elif len(adjList[neighbor]) == 1:
+                        to_be_deleted_2.append(neighbor)         
+            to_be_deleted, to_be_deleted_2 = to_be_deleted_2, []
         
-n = 4
-edges = [[1, 0], [1, 2], [1, 3]]
 
-#n = 6
-#edges = [[0, 3], [1, 3], [2, 3], [4, 3], [5, 4]]
+n = 7
+edges = [[0,1],[1,2],[1,3],[2,4],[3,5],[4,6]]
+
 
 t_s = time.time()
 r = Solution().findMinHeightTrees(n, edges)
