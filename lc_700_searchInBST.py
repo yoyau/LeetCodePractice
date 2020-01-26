44c0b64d9f824cdccb249281121700f1dd667098
yoyau <yoyau.eo06g@nctu.edu.tw>
2020-01-26 12:48:28 +0800

    lc 700 done

---
+++ lc_700_searchInBST.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+700. Search in a Binary Search Tree
+"""
+
+class Solution(object):
+    def searchBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        if not root: return
+        
+        if root.val == val:
+            return root
+        elif root.val > val:
+            return self.searchBST(root.left, val)
+        else:
+            return self.searchBST(root.right, val)
+        
---
+++ lc_797_pathFromS2T.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+797. All Paths From Source to Target
+"""
+
+class Solution(object):
+    def allPathsSourceTarget(self, graph):
+        """
+        :type graph: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        
