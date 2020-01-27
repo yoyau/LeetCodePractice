3d47c6e44ab08f859ea2a82e3f9c8df2e352d84c
yoyau <yoyau.eo06g@nctu.edu.tw>
2020-01-27 18:14:19 +0800

    lc 797 done

--- lc_797_pathFromS2T.py
+++ lc_797_pathFromS2T.py
@@ -9,4 +9,23 @@ class Solution(object):
         :type graph: List[List[int]]
         :rtype: List[List[int]]
         """
+        # graph is already in the "outList" form
+        return self.path(0, graph)
+
+    def path(self, start, graph):
+        ps = []
+        for neighbor in graph[start]:
+            p = [start]
+            if not graph[neighbor]:
+                p.append(neighbor)
+                ps.append(p)
+            else:
+                future_path = self.path(neighbor, graph)
+                for fp in future_path:
+                    ps.append(p+fp)
+        return ps
+
+     
         
+input_ = [[1,2], [3], [], []] 
+o = Solution().allPathsSourceTarget(input_)
