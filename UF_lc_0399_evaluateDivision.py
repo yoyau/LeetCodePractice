f76b8920a7ffd5b310274fe6d511ce109325f59b
yoyau <yoyau.eo06g@nctu.edu.tw>
2020-01-28 19:01:27 +0800

    lc 0399 undone

--- UF_lc_0399_evaluateDivision.py
+++ UF_lc_0399_evaluateDivision.py
@@ -11,8 +11,28 @@ class Solution(object):
         :type queries: List[List[str]]
         :rtype: List[float]
         """
+        equations += [[i, j] for j, i in equations]
+        values += [1/v for v in values]
+        
+        self._outList = dict()
+        for i, eqa in enumerate(equations):
+            if eqa[0] in self._outList:
+                self._outList[eqa[0]][eqa[1]] = values[i]
+            else:
+                self._outList[eqa[0]] = {eqa[1]:values[i]}
+        ans = []
+        for q in queries:
+            ans.append(self.divide(queries[0], queries[1]))
+        return ans
+    
+    def divide(self, i, j):
+        if self._outList[i][j]:
+            return self._outList[i][j]
+        else:
+            for neighbor in self._outList[i]:
+                self.divide(neighbor, j)
 
-equations = [ ["a", "b"], ["b", "c"] ],
-values = [2.0, 3.0],
+equations = [ ["a", "b"], ["b", "c"] ]
+values = [2.0, 3.0]
 queries = [ ["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"] ]
 r = Solution().calcEquation(equations, values, queries)
