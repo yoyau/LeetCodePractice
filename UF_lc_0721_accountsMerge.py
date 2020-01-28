2cfbf1f0e67aef4d3c51129e55ed598ed49f8cd3
yoyau <yoyau.eo06g@nctu.edu.tw>
2020-01-29 00:17:30 +0800

    lc 0721 undone

--- UF_lc_0721_accountsMerge.py
+++ UF_lc_0721_accountsMerge.py
@@ -9,11 +9,59 @@ class Solution(object):
         :type accounts: List[List[str]]
         :rtype: List[List[str]]
         """
+        acc_num = len(accounts)
+        self._parent = list(range(acc_num))
+        self._rank = [0]*acc_num
         
+        self._name = []
+        self._mail = []
+        
+        for acc in accounts:
+            self._name.append(acc[0])
+            self._mail.append(acc[1:])
+        
+        for i in range(len(self._name)):
+            for j in range(i):
+                for mail in self._mail[j]:
+                    if mail in self._mail[i]: 
+                        self.merge(i, j)
+                        break
+        new_acc = []
+        for i, v in enumerate(self._parent):
+            if i == v:
+                mail = list(set(self._mail[i]))
+                mail.sort()
+                new_acc.append([self._name[i]]+mail)
+        print(self._parent)
+        print(self._mail)
+        return new_acc
+      
+    def find(self, x):
+        if x != self._parent[x]:
+            self._parent[x] = self.find(self._parent[x])
+        return self._parent[x]
+    
+    def merge(self, x, y):
+        p_x = self.find(x)
+        p_y = self.find(y)
+        print(p_x, p_y)
+        if p_y == p_x:
+            return
+        if self._rank[p_x] == self._rank[p_y]:
+            self._rank[p_x] += 1
+            self._parent[p_y] = p_x
+            self._mail[p_x]+=self._mail[p_y]
+        elif self._rank[p_x] > self._rank[p_y]:
+            self._parent[p_y] = p_x
+            self._mail[p_x]+=self._mail[p_y]
+        else:
+            self._parent[p_x] = p_y
+            self._mail[p_y]+=self._mail[p_x]
+accounts = [["David","David0@m.co","David4@m.co","David3@m.co"],
+            ["David","David0@m.co"],
+            ["David","David1@m.co","David4@m.co","David0@m.co"],
+            ["David","David0@m.co","David1@m.co","David3@m.co"],
+            ["David","David4@m.co","David1@m.co","David3@m.co"]]
 
-accounts = [["John", "johnsmith@mail.com", "john00@mail.com"], 
-            ["John", "johnnybravo@mail.com"], 
-            ["John", "johnsmith@mail.com", "john_newyork@mail.com"], 
-            ["Mary", "mary@mail.com"]]
-
-r = Solution().accountsMerge(accounts)
+s = Solution()
+r = s.accountsMerge(accounts)
