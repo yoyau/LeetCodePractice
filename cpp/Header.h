76e29eb0720e3d6e652394a364223b61c872ee85
yoyau <yoyau.eo06g@nctu.edu.tw>
2020-02-08 14:41:18 +0800

    _

---
+++ cpp/Header.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+void printsth(void)
+{
+	cout << "aaaa";
+	return;
+}
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,8 +1,11 @@
 
 
 #include <iostream>
+#include "Header.h"
 
 int main()
 {
     std::cout << "Hello World!\n";
+    printsth();
+    return 0;
 }
