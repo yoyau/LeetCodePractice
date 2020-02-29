7757891ce5c775a7d7a53104f87f738bb31020c2
yoyau <yoyau.eo06g@nctu.edu.tw>
2020-02-29 13:58:17 +0800

    lc 0377 done (cpp)

--- cpp/lc_0039_combinationSum.h
+++ cpp/lc_0039_combinationSum.h
@@ -10,9 +10,8 @@ public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> set;
         vector<int> cur;
-        unsigned int start = 0;
         std::sort(candidates.begin(), candidates.end());
-        combinationSearch(candidates, target, cur, start, set);
+        combinationSearch(candidates, target, cur, 0, set);
         return set;
     }
     //
@@ -20,24 +19,36 @@ public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         vector<vector<int>> set;
         vector<int> cur;
-        int start = 0;
         std::sort(candidates.begin(), candidates.end());
-        combinationSearch(candidates, target, cur, start, set);
+        combinationSearch(candidates, target, cur, 0, set);
         std::set<vector<int>> uni(set.begin(), set.end());
         set.assign(uni.begin(), uni.end());
         return set;
     }
     //
     // In combinationSum_III, the length of the combinatio is limited.
-    vector<vector<int>> combinationSum3(vector<int>& candidates, int target) {
+    vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int>> set;
-        vector<int> cur;
-        int start = 0;
-        std::sort(candidates.begin(), candidates.end());
-        combinationSearch(candidates, target, cur, start, set);
-        std::set<vector<int>> uni(set.begin(), set.end());
-        set.assign(uni.begin(), uni.end());
+        vector<int> cur, candidates{ 1,2,3,4,5,6,7,8,9 };
+        combinationSearch(candidates, n, k, cur, 0, set);
         return set;
+    }
+    //
+    // In combination_VI, the same elements combination with different order is considered as different solution.
+    // Only ask the "number" of the solution -> DP problem.
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> sol(target + 1, -1);
+        sol[0] = 1;
+        return helper(nums, target, sol);
+    }
+    int helper(vector<int>& nums, int target, vector<int> & sol) {
+        if (target < 0) return 0;
+        if (sol[target] != -1) return sol[target];
+        int ans = 0;
+        for (auto n : nums) {
+            ans += helper(nums, target - n, sol);
+        }
+        return sol[target] = ans;
     }
 
 private:
@@ -49,7 +60,20 @@ private:
         for (s;s < candidates.size();++s) {
             if (candidates[s] > target) break;
             cur.push_back(candidates[s]);
-            combinationSearch(candidates, target - candidates[s], cur, s+1, set);
+            combinationSearch(candidates, target - candidates[s], cur, s, set);
+            cur.pop_back();
+        }
+    }
+    void combinationSearch(vector<int>& candidates, int target, int maxLength, vector<int> &cur, unsigned int s, vector<vector<int>> &set) {
+        if (!maxLength) {
+            if (!target) 
+                set.push_back(cur);
+        return;
+        }
+        for (s;s < candidates.size();++s) {
+            if (candidates[s] > target) break;
+            cur.push_back(candidates[s]);
+            combinationSearch(candidates, target - candidates[s], maxLength-1, cur, s+1, set);
             cur.pop_back();
         }
     }
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -10,11 +10,11 @@ void scanVV(vector<vector<int>>&);
 int main()
 {
     // creat linked list
-    vector<int> v{ 10,1,2,7,6,1,5 };
+    vector<int> v{ 1,2,3 };
     vector<ListNode> linkedL(v.size(), 10);
     creatLL(v, linkedL);
 
-    vector<int> v2{ 5,6,4 };
+    vector<int> v2{ 3,33,333 };
     vector<ListNode> linkedL2(v2.size(), 10);
     creatLL(v2, linkedL2);
 
@@ -24,8 +24,8 @@ int main()
     cout << endl << "after fun" << endl;
 
     Solution s;
-    vector<vector<int>> a = s.combinationSum2(v, 8);
-    scanVV(a);
+    cout << s.combinationSum4(v2, 10000);
+    //scanVV(a);
 
     // scanLL(&linkedL[0);
     return 0;
@@ -41,9 +41,10 @@ void scanLL(ListNode* head) {
 
 void scanVV(vector<vector<int>>& vec) {
     for (auto v : vec) {
+        cout << "[";
         for (auto e : v) {
-            cout << e;
+            cout << e << ", ";
         }
-        cout << endl;
+        cout << "]" << endl;
     } 
 }
